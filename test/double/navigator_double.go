71874d8f771694b6f4bce9dc80ff5e8cb95114b0
Jordan MacDonald <jordan@wastedintelligence.com>
2014-02-20 23:50:29 -0500

    Add documentation to doubles.

--- test/double/navigator_double.go
+++ test/double/navigator_double.go
@@ -1,5 +1,8 @@
+// The double package provides interface implementations
+// used for integration testing message expectations.
 package double
 
+// Build a struct to track messages/method calls
 type Navigator struct {
 	SelectNextEntryCalled     bool
 	SelectPreviousEntryCalled bool
@@ -7,6 +10,7 @@ type Navigator struct {
 	ToParentDirectoryCalled   bool
 }
 
+// Implement the Navigator interface methods as defined in input.go
 func (n *Navigator) SelectNextEntry() {
 	n.SelectNextEntryCalled = true
 }
--- test/double/reader_double.go
+++ test/double/reader_double.go
@@ -1,7 +1,9 @@
 package double
 
+// Alias the string type to create a fake data source
 type Reader string
 
+// Implement the Reader interface, as required by input.go
 func (s Reader) Read(target []byte) (int, error) {
 	return copy(target, s), nil
 }
