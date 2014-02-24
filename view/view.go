396e586d77cc510d72b0fd393cd63e1cf2c04a3f
Jordan MacDonald <jordan@wastedintelligence.com>
2014-02-23 20:50:44 -0500

    Replaced View()'s return value with a "Row" struct.

--- view/view.go
+++ view/view.go
@@ -5,5 +5,15 @@ data formatting and display updates (using termbox).
 package view
 
 type Viewer interface {
-	View(maxRows uint16) [][2]string
+	View(maxRows uint16) []Row
+}
+
+// Encapsulates information require to draw a row of information.
+//
+// Left and right represent two columns with matching alignment.
+// Highlight inverts the row's colours, useful for "selecting" a row.
+type Row struct {
+	Left      string
+	Right     string
+	Highlight bool
 }
