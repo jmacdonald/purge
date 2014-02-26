e5d9bb8c307ff1b2bd7e39b35e911c08f31f0621
Jordan MacDonald <jordan@wastedintelligence.com>
2014-02-26 09:23:38 -0500

    Add Renderer interface to view, and view.Render() shell.

--- view/view.go
+++ view/view.go
@@ -8,12 +8,28 @@ type Viewer interface {
 	View(maxRows uint16) []Row
 }
 
-// Encapsulates information require to draw a row of information.
-//
-// Left and right represent two columns with matching alignment.
-// Highlight inverts the row's colours, useful for "selecting" a row.
+/*
+Renderer defines the expectations for a display endpoint,
+such that view.Render can properly the data passed to it.
+*/
+type Renderer interface {
+	Flush()
+	Clear()
+	Size() (int, int)
+	SetCell(int, int, rune, termbox.Attribute, termbox.Attribute)
+}
+
+/*
+Encapsulates information require to draw a row of information.
+
+Left and right represent two columns with matching alignment.
+Highlight inverts the row's colours, useful for "selecting" a row.
+*/
 type Row struct {
 	Left      string
 	Right     string
 	Highlight bool
+}
+
+func Render(data Viewer, output Renderer) {
 }
