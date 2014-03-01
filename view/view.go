7e2dc0622bd09f686cca33cebf9a1441389884a0
Jordan MacDonald <jordan@wastedintelligence.com>
2014-03-01 01:11:07 -0500

    Implemented FormatRow, removed partial Render implementation.

--- view/view.go
+++ view/view.go
@@ -4,21 +4,15 @@ data formatting and display updates (using termbox).
 */
 package view
 
+import "fmt"
 import "github.com/nsf/termbox-go"
 
+/*
+Viewer is an interface used by Render to standardize data
+from a data source such that it can be displayed properly.
+*/
 type Viewer interface {
 	View(maxRows uint16) []Row
-}
-
-/*
-Renderer defines the expectations for a display endpoint,
-such that view.Render can properly the data passed to it.
-*/
-type Renderer interface {
-	Flush()
-	Clear()
-	Size() (int, int)
-	SetCell(int, int, rune, termbox.Attribute, termbox.Attribute)
 }
 
 /*
@@ -33,7 +27,25 @@ type Row struct {
 	Highlight bool
 }
 
-func Render(data Viewer, output Renderer) {
-	output.Clear()
-	output.Flush()
+/*
+FormatRow returns a string with the row's left/right
+elements placed at the far left/right with spaces in between.
+*/
+func FormatRow(row Row, size int) (string, error) {
+	// Figure out how large the left field needs to be, including
+	// padding, to have the right field properly aligned to size.
+	leftSize := size - len(row.Right)
+
+	// Don't bother trying to format this row if the left and
+	// right columns can't be separated by at least one space.
+	if leftSize <= len(row.Left) {
+		return "", fmt.Errorf("view: formatting row to a size of %d"+
+			" with '%s' and '%s' values is impossible", size, row.Left, row.Right)
+	}
+
+	// Generate a format string with the appropriate spacing.
+	formatString := fmt.Sprintf("%%-%ds%%s", leftSize)
+
+	// Generate and return the formatted row.
+	return fmt.Sprintf(formatString, row.Left, row.Right), nil
 }
