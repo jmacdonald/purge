c51c1711cf00880c194ec88429f228f2fa7dd731
Jordan MacDonald <jordan@wastedintelligence.com>
2014-03-01 22:37:40 -0500

    Initial (non-rendering) implementation.

--- liberator.go
+++ liberator.go
@@ -1,19 +1,40 @@
 package main
 
-import "github.com/nsf/termbox-go"
-import "time"
-import "os"
+import (
+	"github.com/jmacdonald/liberator/filesystem/directory"
+	"github.com/jmacdonald/liberator/input"
+	"github.com/jmacdonald/liberator/view"
+	"github.com/nsf/termbox-go"
+	"os"
+)
 
 func main() {
 	// Initialize a navigator in the current directory.
-	nav := directory.NewNavigator(os.Getwd())
+	currentPath, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	nav := directory.NewNavigator(currentPath)
+
+	// Set up the terminal screen.
+	err = termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
 
 	// main application loop
 	for {
 		// Read a character from STDIN.
 		character := input.Read(os.Stdin)
 
-		// Invoke the correspoding navigator action
-		input.Map(character, nav)
+		// Invoke the correspoding navigator action,
+		// and exit the main loop if it returns true (exit request).
+		if input.Map(character, nav) {
+			break
+		}
+
+		// Refresh the view.
+		view.Render(nav)
 	}
 }
